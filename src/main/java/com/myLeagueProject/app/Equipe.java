5ff5ee8711164c03744ac3319182fccd06f42f24
Christellew <christelle.wonkam@gmail.com>
2019-05-02 23:26:46 +0200

    depot codeClement&Samah Equipe.java par christelle

---
+++ src/main/java/com/myLeagueProject/app/Equipe.java
@@ -0,0 +1,21 @@
+package com.myLeagueProject.app;
+
+public class Equipe {
+String nomEquipe;
+Joueur j1;
+Joueur j2;
+Joueur j3;
+Joueur j4;
+Joueur j5;
+public Equipe(String nomEquipe, Joueur j1, Joueur j2, Joueur j3, Joueur j4, Joueur j5) {
+	super();
+	this.nomEquipe = nomEquipe;
+	this.j1 = j1;
+	this.j2 = j2;
+	this.j3 = j3;
+	this.j4 = j4;
+	this.j5 = j5;
+}
+
+
+}
