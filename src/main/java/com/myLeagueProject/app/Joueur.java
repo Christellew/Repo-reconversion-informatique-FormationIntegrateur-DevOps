0260c9899c75f0005aa15a03e6713df675d121b9
Christellew <christelle.wonkam@gmail.com>
2019-05-02 23:27:46 +0200

    depot codeClement&Samah Joueur.java par christelle

---
+++ src/main/java/com/myLeagueProject/app/Joueur.java
@@ -0,0 +1,23 @@
+package com.myLeagueProject.app;
+
+public class Joueur {
+String nom;
+String prenom;
+int age;
+int vitesse;
+int technique;
+String poste;
+
+public Joueur(String nom, String prenom, int age, int vitesse, int technique, String poste) {
+	super();
+	this.nom = nom;
+	this.prenom = prenom;
+	this.age = age;
+	this.vitesse = vitesse;
+	this.technique = technique;
+	this.poste = poste;
+}
+
+
+
+}
